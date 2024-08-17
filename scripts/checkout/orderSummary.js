35b5d7f8d8a4a779914a47a06a3d46caf241d603
MdKhalid <mdkhalidsaifullah871@gmail.com>
2024-08-17 21:04:39 +0530

    make the payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {products, getProduct} from "../../data/products.js";
 import {formatCurrency} from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from "../../data/deliveryOptions.js";
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary(){
 
@@ -119,6 +120,7 @@ export function renderOrderSummary(){
 
                 container.remove(); 
                 updateCartQuantity();
+                renderPaymentSummary();
             });
         });
 
@@ -155,6 +157,7 @@ export function renderOrderSummary(){
                 quantityLabel.innerHTML = newQuantity;
             
                 updateCartQuantity();
+                renderPaymentSummary();
             });
         });
 
@@ -164,6 +167,7 @@ export function renderOrderSummary(){
                 const {productId, deliveryOptionId} = element.dataset;
                 updateDeliveryOption(productId, deliveryOptionId);
                 renderOrderSummary();
+                renderPaymentSummary();
             });
         });
 }
